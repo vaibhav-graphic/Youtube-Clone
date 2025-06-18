4a1a0d0abb37f801e3e713bd826212c37b752069
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-06-18 17:31:39 +0530

    add button to the main contanier

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+const Button = ({name}) => {
+  return <div className="py-2 px-8 m-2 bg-gray-200 rounded-lg">
+    <button>{name}</button>
+  </div>;
+};
+
+export default Button;
--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,7 +1,26 @@
+import Button from "./Button";
+
 const ButtonList = () => {
-    return <div>
-        ButtonList
+  const lists = [
+    "All",
+    "Music",
+    "Cooking",
+    "Anime",
+    "Vlog",
+    "Gaming",
+    "Podcast",
+    "Skill",
+    "Trailers",
+    "Live",
+    "Avengers",
+  ];
+  return (
+    <div className="flex">
+      {lists.map((items, index) => (
+        <Button key={index} name={items} />
+      ))}
     </div>
-}
+  );
+};
 
 export default ButtonList;
